dc8b7140d8a50a992d2e82e5975c57fb305b4915
rachel.howard@altran.com <rachel.howard@altran.com>
2020-09-14 16:55:01 +0100

    Part 3 working. It now sorts and prints domains with higher frequency than a user entered value

--- email extraction/email_extraction_dictionary.py
+++ email extraction/email_extraction_dictionary.py
@@ -14,9 +14,24 @@ with open(filename) as file:
         domain = re.search("@\S+", list[i]).group(0)
         #Add to dictionary / increment entry
         freq_dict[domain] = freq_dict.get(domain, 0) + 1
-    #ToDo: sort
-    sorted(freq_dict)
 
     #print dictionary
+    print("Dictionary")
     for domain, frequency in freq_dict.items():
         print(domain, frequency)
+
+    print("----------------------------")
+    print("sorted:")
+    # dictionary is now a list of domains in decending frequency order
+    freq_dict_sorted_list = [domain for domain, freq in sorted(freq_dict.items(), key=lambda item: item[1], reverse=True)]
+    for i in range(10):
+        print(freq_dict_sorted_list[i])
+
+
+    #no check of input. dangerous to proceed
+    print("----------------------------")
+    userinput = input("Enter a number and domains with a higher frequency than it will be returned (unordered): ")
+    for domain, freq in freq_dict.items():
+        if freq >= int(userinput):
+            print(domain, freq)
+

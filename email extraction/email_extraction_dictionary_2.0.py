3468fbe6110ac5a1550787d14f49c04eead39af5
rachel.howard@altran.com <rachel.howard@altran.com>
2020-09-14 17:31:21 +0100

    Building from email_extraction_dictionary, this new file only considers higher domains. For example hotmail is the sum of all the hotmail.com and hotmail.co.uk email addresses

---
+++ email extraction/email_extraction_dictionary_2.0.py
@@ -0,0 +1,21 @@
+import re
+from typing import Dict
+
+filename = "sample.txt"
+#freq_dict : Dict[domain :str, frequency: num]
+freq_dict = {}
+
+with open(filename) as file:
+    data = file.read()
+    #find all emails
+    list = re.findall('\S+@\S+', data)
+
+    for i in range(len(list)):
+        domain = re.search("@\S+?\.", list[i]).group(0)[:-1]
+        #Add to dictionary / increment entry
+        freq_dict[domain] = freq_dict.get(domain, 0) + 1
+
+    #print dictionary
+    print("Dictionary")
+    for domain, frequency in freq_dict.items():
+        print(domain, frequency)
